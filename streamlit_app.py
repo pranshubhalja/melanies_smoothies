3aab47d0375f87e03388658f9c3fc3b08242b9be
Pranshu Bhalja <pranshu.bhalja@gmail.com>
2026-05-29 09:36:04 +0530

    Update streamlit_app.py
    Made changes for pd_df error

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,10 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your current Smoothie will be:", name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
+pd_df = my_dataframe.to_pandas()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
-ingredients_list= st.multiselect('Choose upto 5 ingredients:',my_dataframe,max_selections=5)
+ingredients_list= st.multiselect('Choose upto 5 ingredients:',pd_df['fruit_name'],max_selections=5)
 if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
@@ -24,7 +25,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen+' Nutrition Information')
-        smoothiefroot_response = requests.get ("https://my.smoothiefroot.com/api/fruit/{search_on}")
+        smoothiefroot_response = requests.get (f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders (ingredients,name_on_order) values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
